4782617d5e411d7a808b161c0ef1c3ce6c85b178
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2026-02-15 12:30:09 +0530

    README.md Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div>
-            <h1>🎓 Kavithma NextGen Academy 🎓</h1>
+            <h1 className="text-red-500">🎓 Kavithma NextGen Academy 🎓</h1>
         </div>
     )
 }
